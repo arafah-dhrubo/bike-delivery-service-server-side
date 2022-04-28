d430e2e89dddfb37acdcdf5745c96b5c9594dfdb
samiulpro <samiul15-13051@diu.edu.bd>
2022-04-28 23:31:08 +0600

    Updated

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wgl4i.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER)
+const uri = `mongodb+srv://dhrubo13051:${process.env.PASSWORD}@cluster0.wgl4i.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
